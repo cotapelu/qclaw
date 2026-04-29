46b8f027f1e6f2fda71cd9f6136e7e0e4a637ebf
quangtynu <quangbq@gmail.com>
2026-04-29 14:23:47 +0700

    Revert "feat: Add session tool for multi-session management"
    This reverts commit 88df8a991ba1128b13f1be6ae09e7f3c7ff64b0b.

--- src/extensions/tools/index.ts
+++ src/extensions/tools/index.ts
@@ -3,6 +3,5 @@
  * Re-exports all custom tools for registration
  */
 
-export { registerSessionsTool } from "./session-tool.js";
 export { registerTodosTool } from "./todos-tool.js";
 export { registerMemoryTool } from "./memory-tool.js";
